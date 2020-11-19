5d19ab54fc5e058227f724aef8cd49037eb682db
Putri Mutiara Tungga <putri.mutiara@bukalapak.com>
2020-11-19 19:00:48 +0700

    cleaning playground.go

---
+++ go.mod
@@ -0,0 +1,10 @@
+module example.com
+
+go 1.15
+
+require (
+	cloud.google.com/go/storage v1.12.0 // indirect
+	github.com/gocql/gocql v0.0.0-20201024154641-5913df4d474e
+	github.com/julienschmidt/httprouter v1.3.0
+	google.golang.org/api v0.35.0 // indirect
+)
--- playground.go
+++ playground.go
@@ -1,31 +1,4 @@
 package main
 
-import (
-	"context"
-	"fmt"
-)
-
-type ctxKey string
-
-// CobaContext coba
-type CobaContext struct {
-	context context.Context
-}
-
-// FuncCobaContext coba
-func FuncCobaContext() {
-	ctx := context.TODO()
-	ctx = context.WithValue(ctx, ctxKey("X-Request-ID"), "hahahaha")
-	fmt.Println(ctx.Value(ctxKey("X-Request-ID")).(string))
-
-	a := CobaContext{
-		// context: ctx,
-	}
-	fmt.Println(a.context == nil)
-	// reqID := a.context.Value(ctxKey("X-Request-ID")).(string)
-	// fmt.Println(reqID)
-}
-
 func main() {
-	FuncCobaContext()
 }
